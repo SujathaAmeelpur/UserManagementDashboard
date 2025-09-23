8b1648b8e18b440e6f04b7d38a0ef7a867ef65b5
SujathaAmeelpur <sujathaameel@gmail.com>
2025-09-23 22:32:40 +0530

    Add files via upload

---
+++ User Management Dashboard/user/App.js
@@ -0,0 +1,15 @@
+
+import UserInputForm from './components/UserInputForm'
+import UserDetailsList from './components/UserDetailsList'
+import './App.css';
+
+function App() {
+ return (
+    <div className="App">
+      <UserDetailsList />
+      <UserInputForm/>
+    </div>
+  );
+}
+
+export default App;
