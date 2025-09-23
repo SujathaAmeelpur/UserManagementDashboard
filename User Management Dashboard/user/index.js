20f0763105849fb8cc4706b56cfb2273a0f2a481
SujathaAmeelpur <sujathaameel@gmail.com>
2025-09-23 22:35:43 +0530

    Add files via upload

--- User Management Dashboard/user/index.js
+++ User Management Dashboard/user/index.js
@@ -1,53 +1,67 @@
 
-import {Component} from 'react'
-import './index.css'
 
-class UserInputForm extends Component{
-  state = {
-    firstName:'',
-    lastName:'',
-    email:'',
-    department:'',
-    ShowSubmitErrorMsg:false,
-    errorMsg:'',
-  }
+import React, {useEffect,useState} from 'react'
+function UserDetailsList(){
 
- 
+    const [users, setUsers] =useState([])
+    const [id,setId] = useState("")
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [department, setDepartment] = useState("")
 
-  render(){
+    //fetch users
+    useEffect(()=> {
+        fetch("http://localhost:3000/users")
+        .then(res => res.json())
+        .then(data => setUsers(data));
+    },[]);
+
+    const AddUser = () => {
+        fetch("http://localhost:3000/users", {
+            method:'POST',
+            headers:{"Content-Type":
+                "application/json"
+            },
+            body: 
+            JSON.stringify({id,firstName,lastName,email,department})
+        })
+        .then(res => res.json())
+        .then(newUser => setUsers([...users,newUser]))
+    }
+
+
+
     return(
-      <>
-      <div className='container'>
-        <form className='form-container'>
-          <div>
-            <label htmlfor = "id-input">ID :</label>
-            <input id = "id-input" type = 'text'/>
-          </div>
-          <div>
-            <label htmlfor = "name-input">First Name :</label>
-            <input id = "name-input" type = 'text'/>
-          </div>
-          <div>
-            <label htmlfor = "name-input">Last Name :</label>
-            <input id = "name-input" type = 'text'/>
-          </div>
-          <div>
-            <label htmlfor = "email-input">Email :</label>
-            <input id = "email-input" type = 'text'/>
-          </div>
-          <div>
-            <label htmlfor = "department-input">Department :</label>
-            <input id = "department-input" type = 'text'/>
-          </div>
-          <button type = "submit">Submit</button>
-        </form>
-      </div>
-      </>
-    )
-  }
+        <div>
+            <h1>User Dashboard</h1>
+            <input placeholder='id'
+            onChange={e=> setId(e.target.value)} />
+            <input placeholder='firstName'
+            onChange={e => setFirstName(e.target.value)}
+            />
+            <input placeholder='lastName' 
+            onChange={e => setLastName(e.target.value)}
+            />
+            <input placeholder='email' 
+            onChange={e => setEmail(e.target.value)}
+            />
+            <input placeholder='department' 
+            onChange={e => setDepartment(e.target.value)}
+            />
+            <button onClick={AddUser} >Add</button>
+            <button>Edit</button>
+            <button>Delete</button>
+            <ul>
+                {users.map(user => (
+                    <li key = {user.id}>{user.firstName} {user.lastName} {user.email}</li>
+                ))}
+            </ul>
 
+        </div>
+    );
 
-  
+    
 }
 
-export default UserInputForm
+export default UserDetailsList
