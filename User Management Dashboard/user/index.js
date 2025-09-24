5dc3337ff7528d4105ad956957d9d91bafc14091
SujathaAmeelpur <sujathaameel@gmail.com>
2025-09-24 09:25:34 +0530

    Add files via upload

--- User Management Dashboard/user/index.js
+++ User Management Dashboard/user/index.js
@@ -1,6 +1,7 @@
 
 
 import React, {useEffect,useState} from 'react'
+import './index.css'
 function UserDetailsList(){
 
     const [users, setUsers] =useState([])
@@ -9,15 +10,21 @@ function UserDetailsList(){
     const [lastName, setLastName] = useState("")
     const [email, setEmail] = useState("")
     const [department, setDepartment] = useState("")
+    const [editingUser, setEditingUser] = useState(null)
 
     //fetch users
-    useEffect(()=> {
+    const fetchUsers = ()=> {
         fetch("http://localhost:3000/users")
         .then(res => res.json())
         .then(data => setUsers(data));
-    },[]);
+    };
+    
+    useEffect(()=>{
+         fetchUsers();
+    },
+    []);
 
-    const AddUser = () => {
+    const addUser = () => {
         fetch("http://localhost:3000/users", {
             method:'POST',
             headers:{"Content-Type":
@@ -27,13 +34,50 @@ function UserDetailsList(){
             JSON.stringify({id,firstName,lastName,email,department})
         })
         .then(res => res.json())
-        .then(newUser => setUsers([...users,newUser]))
-    }
+        .then(()=> {
+            fetchUsers();
+            setFirstName("");
+            setLastName("");
+            setEmail("");
+            setDepartment("");
+        });
+    };
+
+    const deleteUser = (id) => {
+        fetch(`http://localhost:3000/users/${id}`,{method:'Delete'})
+        .then(() => fetchUsers());
+    };
+
+    const startEditing = (user) => {
+        setEditingUser(user);
+        setFirstName(user.firstName);
+        setLastName(user.lastName);
+        setEmail(user.email);
+        setDepartment(user.department);
+    };
+
+    const updateUser = () => {
+        fetch(`http://localhost:3000/users/${editingUser.id}`,{
+            method:"PUT",
+            headers:{"Content-Type":"application/json"},
+            body:
+            JSON.stringify({firstName,lastName,email,department})
+        })
+        .then(() => {
+            fetchUsers();
+            setEditingUser(null);
+            setFirstName("");
+            setLastName("");
+            setEmail("");
+            setDepartment("");
+            
+        });
+    };
 
 
 
     return(
-        <div>
+        <div className='bg-container'>
             <h1>User Dashboard</h1>
             <input placeholder='id'
             onChange={e=> setId(e.target.value)} />
@@ -49,12 +93,19 @@ function UserDetailsList(){
             <input placeholder='department' 
             onChange={e => setDepartment(e.target.value)}
             />
-            <button onClick={AddUser} >Add</button>
-            <button>Edit</button>
-            <button>Delete</button>
+
+            {editingUser ? (
+                <button onClick={updateUser}>Update User</button>
+            ) : (<button onClick={addUser}>Add</button> )}
+            
+           
+            
             <ul>
                 {users.map(user => (
-                    <li key = {user.id}>{user.firstName} {user.lastName} {user.email}</li>
+                    <li key = {user.id}>{user.firstName} {user.lastName} {user.email}
+                     <button onClick={()=> startEditing(user)} >Edit</button>
+                     <button onClick={()=> deleteUser(user.id)} >Delete</button>
+                    </li>
                 ))}
             </ul>
 
